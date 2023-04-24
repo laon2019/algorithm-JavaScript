80d9f63d9e177a1984f89394f434bcd6bc306582
laon2019 <laon5987@nate.com>
2023-04-24 23:37:06 +0900

    0424

---
+++ PG_LV1_13.js
@@ -0,0 +1,11 @@
+//자릿수 더하기
+function solution(n)
+{
+    var answer = 0;
+    var m = String(n);
+    
+    for ( let i = 0; i < m.length; i++) {
+        answer += parseInt(m[i]);
+    }
+    return answer;
+}
---
+++ PG_LV2_7.js
@@ -0,0 +1,13 @@
+let getLCM = (num1, num2) =>{
+	let lcm = 1;
+   
+    while(true){
+      if((lcm % num1 == 0) && (lcm % num2 == 0)){
+        break;
+      }
+      lcm++;
+    }
+  	return lcm
+}
+
+console.log(getLCM(8,10))
