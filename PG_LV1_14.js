36ceebc251b97530e4d9624ae09d24edbb70d778
laon2019 <laon5987@nate.com>
2023-04-25 13:32:25 +0900

    0425

---
+++ PG_LV1_14.js
@@ -0,0 +1,21 @@
+//두 정수 사이의 합
+function solution(a, b) {
+    var answer = 0;
+    if(a===b){
+        return a;
+    }else{
+        if(a>b){
+            for(let i=b;i<=a;i++){
+            answer+=i;
+        }
+        return answer;
+        }else{
+            for(let i=a;i<=b;i++){
+            answer+=i;
+        }
+        return answer;
+        }
+        
+    }
+    
+}
