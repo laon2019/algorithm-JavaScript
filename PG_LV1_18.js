5235c3969baf5b320c3f3c4cd424dcdd7e2f3adc
laon2019 <laon5987@nate.com>
2023-05-02 23:59:26 +0900

    0502

---
+++ PG_LV1_18.js
@@ -0,0 +1,12 @@
+//자연수 뒤집어 배열로 만들기 
+function solution(n) {
+    var arr = n.toString().split('');
+  // n을 string으로 형변환하고 split으로 하나씩 끊어줌.
+    var answer = [];
+
+    for(var i=arr.length-1; i>=0; i--){
+        answer.push(Number(arr[i]));
+    }
+  //arr의 인덱스 역순으로 answer의 배열 안에 넣어준다.
+  return answer;
+}
