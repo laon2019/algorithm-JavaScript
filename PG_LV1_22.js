58abcc3fcac0382005b3beba7b30b5f7ad754036
laon2019 <laon5987@nate.com>
2023-05-11 12:44:37 +0900

    0511

---
+++ PG_LV1_22.js
@@ -0,0 +1,11 @@
+//가운데 글자 가져오기
+function solution(s) {
+    let answer = '';
+    if (s.length % 2 == 0 ) { 
+        answer = s[s.length / 2 - 1] + s[s.length / 2];
+    } else {
+        answer = s[Math.floor(s.length/2)]; 
+    }
+    return answer;
+    return answer;
+}
