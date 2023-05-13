21582bdaf127953864ca33b69f2fa8ce92afa76e
laon2019 <laon5987@nate.com>
2023-05-13 11:19:50 +0900

    0513

---
+++ PG_LV2_12.js
@@ -0,0 +1,18 @@
+//피보나치수
+function solution(n) {
+    let result = [];
+    for(let i =0; i <= n;i++){
+        if ( i == 0 ){
+            result.push(0)
+        }
+        if (i == 1){
+            result.push(1)
+        }
+        if (i >=2){
+            let sum = result[i-2] + result[i-1];
+            result.push(sum % 1234567)
+        }
+    }
+    let answer = result[n] 
+    return answer;
+}
