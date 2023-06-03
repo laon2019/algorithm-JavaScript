67bee671c986fcb4617dcfbae7e4ad8332d6f696
laon2019 <laon5987@nate.com>
2023-06-03 16:13:13 +0900

    0603

---
+++ PG_LV1_26.js
@@ -0,0 +1,12 @@
+//문자열 다루기 기본
+function solution(s) {
+    if (s.length !== 4 && s.length !== 6){
+        return false;
+    } 
+    for (let i = 0; i < s.length; i++) {
+      if (isNaN(s[i])){
+         return false; 
+      } 
+    }
+    return true;
+}
